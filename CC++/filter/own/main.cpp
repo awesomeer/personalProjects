d54f99db08fc5a80da69b361f02f035333eab684
awesomeer <47190013+awesomeer@users.noreply.github.com>
2019-06-06 19:54:57 -0500

    fix

--- CC++/filter/own/main.cpp
+++ CC++/filter/own/main.cpp
@@ -32,8 +32,9 @@ double DFT(double * in, double fs, int points, double ft){
 
     for(int i = 0; i < points; i++){
         double argu = (2.0 * M_PI * (float) i * m) / points;
-        real += in[i] * cos(argu);
-        imag -= in[i] * sin(argu);
+		double w = 0.5 - 0.5*cos(2*M_PI*i/points);
+        real += in[i] * cos(argu) * w;
+        imag -= in[i] * sin(argu) * w;
     }
     
     return sqrt(pow(real,2) + pow(imag,2));
@@ -67,10 +68,10 @@ int main(){
         double mag = DFT(input, 1/dt, n, i)/n;
         out << mag << endl;
     }
-    out << endl << endl;
+    /* out << endl << endl;
     for(int i = 1; i <= 20000; i++){
         out << i << endl;
-    }
+    } */
     out.close();
     return 0;
 }
