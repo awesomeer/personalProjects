c93f475d12da40973db2a51cce6d56f5a486c20f
awesomeer <mithileshkonakmit@gmail.com>
2019-06-20 03:15:07 -0500

    Test

--- CC++/OS/test.c
+++ CC++/OS/test.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void child(){
+	printf("Child\n");
+}
+
+void parent(){
+	printf("Parent\n");
+}
 
 int main(){
 	int x = 3;
-	if(fork() != 0)
-		printf("x = %d\n", ++x);
+	if(fork() == 0){
+		child();
+		printf("pid = %d\n", getpid());
+	}
 	
-	printf("x = %d\n", --x);
+	printf("pid = %d\n", getpid());
 	
 	return 0;
 }
