53df20b046e9ab1a1dd0a0afc83b149afd3b62b7
Mithilesh Konakanchi <mithileshkonakmit@gmail.com>
2019-08-21 03:22:29 -0500

    fix

--- python/bluetooth/sniffer.py
+++ python/bluetooth/sniffer.py
@@ -1,6 +1,14 @@
 import bluetooth
+from time import sleep
 
 devices = bluetooth.discover_devices(lookup_names=True);
 print(str(len(devices)) + " devices found")
 for device in devices:
     print(device)
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((devices[0][0], 1))
+
+sleep(3)
+sock.close()
+
