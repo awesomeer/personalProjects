fa3501404241e0870b5d0f0deda4091abc17dc56
awesomeer <47190013+awesomeer@users.noreply.github.com>
2020-03-15 14:34:04 -0500

    f

---
+++ Microcontroller/AVR/attiny45/ledStrip.X/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: mithilesh
+ *
+ * Created on March 14, 2020, 10:57 PM
+ */
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
+void ISR(PCINT0_vect){
+    
+}
