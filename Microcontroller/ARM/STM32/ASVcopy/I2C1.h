44077079e57ab9fecdcedb75d0408d3a284af4a8
awesomeer <47190013+awesomeer@users.noreply.github.com>
2020-05-16 21:48:22 -0500

    cleanup

--- Microcontroller/ARM/STM32/ASVcopy/I2C1.h
+++
@@ -1,6 +0,0 @@
-#ifndef I2C1_H_
-#define I2C1_H_
-
-void I2C1Init(void);
-
-#endif
--- Microcontroller/ARM/STM32/ASVcopy/Objects/fifo.d
+++
@@ -1,3 +0,0 @@
-.\objects\fifo.o: FIFO.c
-.\objects\fifo.o: FIFO.h
-.\objects\fifo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
