cc0a43823053f16cc5be64b081dfc5c7c01ecc01
awesomeer <47190013+awesomeer@users.noreply.github.com>
2020-08-27 02:37:24 -0500

    f

--- Python/Scratch/sin.py
+++ Python/Scratch/sin.py
@@ -1,12 +1,13 @@
 import math
 
 N = 512
+angle = 4*math.pi/3
 
 for r in range(int(N/100)+1):
     for i in range(100):
         index = r*100+i
         if index >= N:
             break
-        value = 127*math.sin(2*math.pi*index/N)+128
+        value = 127*math.sin(2*math.pi*index/N + angle)+128
         print(str(int(value))+", ", end ="")
     print()
