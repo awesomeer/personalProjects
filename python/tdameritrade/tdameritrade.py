90e9134c27eadec6bd0d6e8b55bd1fd90a9e1094
awesomeer <mithileshkonakmit@gmail.com>
2021-05-12 17:02:19 -0500

    edit

--- python/tdameritrade/tdameritrade.py
+++ python/tdameritrade/tdameritrade.py
@@ -4,7 +4,37 @@ import time
 CLIENT_ID = 'BUEB2RIHCXF2WSJOYSGSVTRAERIAJESR'
 REDIRECT_URL = 'https://www.google.com/'
 CREDENTIALS_PATH = 'token.json'
+SIZE = 400
 
+TDSession = None
+stocks = ''
+stockPrices = dict()
+quotes = dict()
+
+quoteLock = threading.Lock()
+
+def updateQuotes(stocks, quotes, session):
+	
+	quoteLock.acquire()	
+	
+	for i in range(len(stocks)//SIZE):
+		batch = stocks[ i*400 : (i*400)+400 ]
+		quote = session.get_quotes(instrument=batch)
+		quotes.update(quote)
+	
+	remain = len(stocks) % SIZE
+	if remain != 0:
+		quote = session.get_quotes(instruments=stocks[-1*remain:])
+		quotes.update(quote)
+		
+	quoteLock.release()
+
+def updateStockPrices(stockPrices, quotes):
+	for stock in stockPrices:
+		stockPrices[stock] = [quotes[stock]['bidPrice']] + stockPrices[stock][:-1]
+		
+
+# Create session for TD Ameritrade
 TDSession = TDClient(
             client_id = CLIENT_ID, 
             redirect_uri = REDIRECT_URL,
@@ -13,6 +43,33 @@ TDSession = TDClient(
 
 TDSession.login()
 
+
+# Read in the list of stocks to monitor
+with open('all.list', 'r') as f:
+	stocks = f.read()
+	stocks = stocks.split()
+
+updateQuotes(stocks, quotes, session)
+
+
+# Init stockPrices and remove exccess stocks from 'stocks' list (stocks that cause a KeyError)
+for s in range(len(stocks)-1, -1, -1):
+	stock = stocks[s]
+	if stock in quotes:
+		stockPrices[stock] = 900 * [quotes[stock]['bidPrice']]
+	else:
+		stocks.pop(s)
+
+
+quoteTimer = threading.Timer(1.0, updateQuotes, args = (stocks, quotes, TDSession, ))
+quoteTimer.start()
+
+priceChanges = [dict() for i in range(3)]
+while True:
+    
+
+
+"""
 # Get the list of stocks to watch, list of stocks as strings
 stocks = ''
 with open('stocks.list', 'r') as f:
@@ -78,3 +135,4 @@ while True:
 
     print()
     time.sleep(1)
+   """
