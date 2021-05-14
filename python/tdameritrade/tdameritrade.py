cf1d8833d915c55f38a06ff0a216b95597698ca6
awesomeer <mithileshkonakmit@gmail.com>
2021-05-14 11:38:56 -0500

    fixes

--- python/tdameritrade/tdameritrade.py
+++ python/tdameritrade/tdameritrade.py
@@ -1,5 +1,5 @@
 from td.client import TDClient
-import time
+import threading
 
 CLIENT_ID = 'BUEB2RIHCXF2WSJOYSGSVTRAERIAJESR'
 REDIRECT_URL = 'https://www.google.com/'
@@ -9,30 +9,52 @@ SIZE = 400
 TDSession = None
 stocks = ''
 stockPrices = dict()
-quotes = dict()
 
-quoteLock = threading.Lock()
+priceLock = threading.Lock()
+outLock = threading.Lock()
 
-def updateQuotes(stocks, quotes, session):
+def updatePrices():
+
+	global stockPrices
+	global TDSession
+	global stocks
 	
-	quoteLock.acquire()	
-	
+	print('Price Thread')
+	quotes = dict()
 	for i in range(len(stocks)//SIZE):
-		batch = stocks[ i*400 : (i*400)+400 ]
-		quote = session.get_quotes(instrument=batch)
+		batch = stocks[ i*SIZE : (i*SIZE)+SIZE ]
+		quote = TDSession.get_quotes(instruments=batch)
 		quotes.update(quote)
-	
+
 	remain = len(stocks) % SIZE
 	if remain != 0:
-		quote = session.get_quotes(instruments=stocks[-1*remain:])
+		quote = TDSession.get_quotes(instruments=stocks[-1*remain:])
 		quotes.update(quote)
-		
-	quoteLock.release()
 
-def updateStockPrices(stockPrices, quotes):
-	for stock in stockPrices:
-		stockPrices[stock] = [quotes[stock]['bidPrice']] + stockPrices[stock][:-1]
-		
+
+	priceLock.acquire()
+	print('Price Lock acquired')
+
+    # Update 'stockPrices' dict with prices from 'quotes' dict
+	print(len(quotes), len(stocks))
+	for stock in quotes:
+		if quotes[stock]['description'] == 'Symbol not found':
+			continue
+		if (stock in stockPrices):
+			stockPrices[stock] = [quotes[stock]['bidPrice']] + stockPrices[stock][:-1]
+		else:
+			stockPrices[stock] = 901 * [quotes[stock]['bidPrice']]
+    
+    # Update 'stockPrices' dict with stocks not found in 'quotes' dict
+	for stock in quotes.keys() ^ stockPrices.keys():
+		if not (stock in stockPrices):
+			continue
+		stockPrices[stock] = [stockPrices[stock][0]] + stockPrices[stock][:-1]
+
+	print('outLock released')
+	outLock.release()
+
+	threading.Timer(1.0, updatePrices).start()
 
 # Create session for TD Ameritrade
 TDSession = TDClient(
@@ -49,90 +71,38 @@ with open('all.list', 'r') as f:
 	stocks = f.read()
 	stocks = stocks.split()
 
-updateQuotes(stocks, quotes, session)
+outLock.acquire()
+updatePrices()
 
-
-# Init stockPrices and remove exccess stocks from 'stocks' list (stocks that cause a KeyError)
-for s in range(len(stocks)-1, -1, -1):
-	stock = stocks[s]
-	if stock in quotes:
-		stockPrices[stock] = 900 * [quotes[stock]['bidPrice']]
-	else:
-		stocks.pop(s)
-
-
-quoteTimer = threading.Timer(1.0, updateQuotes, args = (stocks, quotes, TDSession, ))
-quoteTimer.start()
-
-priceChanges = [dict() for i in range(3)]
 while True:
+	priceChanges = [list() for i in range(3)]
+
+	outLock.acquire()
+
+	# Calculate Price changes
+	for stock in stockPrices:
+		for index,second in [(0,60), (1, 300), (2, 900)]:
+			curr = stockPrices[stock][0]
+			prev = stockPrices[stock][second]
+			try:
+				perChange = 100 * ((curr-prev)/prev)
+			except ZeroDivisionError:
+				print(stock)
+			priceChanges[index].append( (stock, perChange) )
+		#print(stock, stockPrices[stock][0])
     
+	priceLock.release()
 
+    # Sort 'priceChanges' lists
+	#print(priceChanges)
+	for i in range(3):
+		priceChanges[i] = sorted(priceChanges[i], key = lambda x: x[1], reverse=True)
 
-"""
-# Get the list of stocks to watch, list of stocks as strings
-stocks = ''
-with open('stocks.list', 'r') as f:
-    stocks = f.read()
-    stocks = stocks.split()
-print(stocks)
-stockPrices = dict()
-
-quote = TDSession.get_quotes(instruments=stocks)
-for i in range(len(stocks)-1, -1, -1):
-    stock = stocks[i]
-    print(stock)
-    try:
-        stockPrices[stock] = 900 * [quote[stock]['bidPrice']]
-        print(stock, quote[stock]['bidPrice'])
-    except KeyError:
-        stocks.pop(i)
-        print(stock, 'not found')
-
-priceChanges = [dict() for i in range(3)]
-while True:
-    quote = TDSession.get_quotes(instruments=stocks)
-    print('Now'.center(15) + '1 minute'.center(15) + '5 minutes'.center(15) + '15 minutes'.center(15))
-
-    for stock in stocks:
-
-        curr = 0
-        try:
-            curr = quote[stock]['bidPrice']
-        except KeyError:
-            curr = stockPrices[stock][0]
-            print('KeyError:', stock)
-
-        outString = stock + ': {:.2f}'.format(curr)
-        print(outString.center(15), end='')
-        #print(stock, curr, ' ',  end='')
-
-        for index, second in [(0,59), (1,299), (2,-1)]:
-            prev = stockPrices[stock][second]
-            priceChanges[index][stock] = (100 * (curr - prev)) / prev
-
-            outString = stock + ': {:.2f}'.format(prev)
-            print(outString.center(15), end='')
-            #print(stock, prev, ' ', end='')
-
-        stockPrices[stock] = [curr] + stockPrices[stock][:-1]
-        print()
-
-
-    sortedChanges = 3*[None]
-    for i in [0,1,2]:
-        sortedChanges[i] = sorted(priceChanges[i].items(), key=lambda x: x[1], reverse=True)
-        #print(sortedChanges[i])
-
-    print('1 minute'.center(20) + '5 minutes'.center(20) + '15 minutes'.center(20))
-    for s in range(5 if len(stocks) >= 5 else len(stocks)):
-        for t in range(3):
-            outStr = sortedChanges[t][s][0] + ': {:.2f}'.format(sortedChanges[t][s][1])
-            print(outStr.center(20), end='')
-        print()
-
+	print('Now'.center(15) + '1 minute'.center(15) + '5 minutes'.center(15) + '15 minutes'.center(15))
+	for rank in range(5):
+		print(' '.center(15), end='')
+		for time in range(3):
+			outStr = priceChanges[time][rank][0] + ': {:.2f}'.format(priceChanges[time][rank][1])
+			print(outStr.center(15), end='')
+		print()
     
-
-    print()
-    time.sleep(1)
-   """
