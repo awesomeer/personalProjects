a70007fc13beb1687b8fbc731ea5cb3543c726a1
awesomeer <mithileshkonakmit@gmail.com>
2021-05-18 12:46:58 -0500

    less space

--- C++/kernel/createImage.py
+++ C++/kernel/createImage.py
@@ -13,9 +13,9 @@ outfile.close()
 os.system('ndisasm -u kernel-short.bin > kernel.dis')
 os.system('cat ../OS/boot_sect.bin kernel-short.bin > os-image')
 
-infile = open('os-image', 'ab')
-infile.write(bytearray(0x1000))
-infile.close
+#infile = open('os-image', 'ab')
+#infile.write(bytearray(0x1000))
+#infile.close
 
 os.system('rm kernel-short.bin')
 os.system('rm kernel.bin')
